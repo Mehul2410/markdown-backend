89344d1045c2314f5de98d6b4e7945b2d17ff937
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-07-06 00:57:00 +0530

    done

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -8,6 +8,30 @@
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
+  async updateMe(ctx) {
+    let entity;
+    // get authenticated user details
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorized header found" }] },
+      ]);
+    }
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data["user"] = user;
+      entity = await strapi.services.profile.update({ user: user.id }, data, {
+        files,
+      });
+    } else {
+      const data = ctx.request.body;
+      data["user"] = user;
+      entity = await strapi.services.profile.update({ user: user.id }, data);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.profile });
+  },
+
   async findMe(ctx) {
     let entities;
     //get authenticated user details
--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +10,16 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+  // import socket io
+  //   var io = require("socket.io")(strapi.server);
+  //   // listen for user connection
+  //   io.on("connection", function (socket) {
+  //     // send message on user connection
+  //     socket.emit("hello", JSON.stringify({ message: "Hello food lover" }));
+  //     // listen for user diconnect
+  //     socket.on("disconnect", () => console.log("a user disconnected"));
+  //   });
+  //   strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
+  //   cb;
+};
