c23a32a4ec712a8c62ca6686843eed79f82ba51d
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-07-07 00:35:30 +0530

    final push

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 "use strict";
-=======
-'use strict';
->>>>>>> ac26fbd481bf60e0658d79ed3c5225e568c0f9f5
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -14,20 +10,4 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-<<<<<<< HEAD
-module.exports = () => {
-  // import socket io
-  //   var io = require("socket.io")(strapi.server);
-  //   // listen for user connection
-  //   io.on("connection", function (socket) {
-  //     // send message on user connection
-  //     socket.emit("hello", JSON.stringify({ message: "Hello food lover" }));
-  //     // listen for user diconnect
-  //     socket.on("disconnect", () => console.log("a user disconnected"));
-  //   });
-  //   strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
-  //   cb;
-};
-=======
 module.exports = () => {};
->>>>>>> ac26fbd481bf60e0658d79ed3c5225e568c0f9f5
--- config/server.js
+++ config/server.js
@@ -1,13 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-<<<<<<< HEAD
-      secret: env('ADMIN_JWT_SECRET', '90f19aef1696f51e9645a2fd98b035f0'),
-=======
-      secret: env('ADMIN_JWT_SECRET', 'bd83e08d1f7419cb051641c2bf3f93cf'),
->>>>>>> ac26fbd481bf60e0658d79ed3c5225e568c0f9f5
+      secret: env("ADMIN_JWT_SECRET", "bd83e08d1f7419cb051641c2bf3f93cf"),
     },
   },
 });
